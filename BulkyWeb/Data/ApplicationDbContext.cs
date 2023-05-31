7b728ac7c632304ce4bf4f27a626e5d305569435
Nasrullayev Alisher <myemail9445@inbox.ru>
2023-05-31 11:59:08 +0500

    Postgresql connection and category model

---
+++ BulkyWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BulkyWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyWeb.Data
+{
+	public class ApplicationDbContext : DbContext
+	{
+		protected readonly IConfiguration _configuration;
+		public ApplicationDbContext(IConfiguration configuration)
+		{
+			_configuration = configuration;
+		}
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseNpgsql(_configuration.GetConnectionString("DefaultConnection"));
+		}
+
+		public DbSet<Category> categories { get; set; }
+
+	}
+}
---
+++ BulkyWeb/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyWeb.Models
+{
+	public class Category
+	{
+        // [Key] // boshqa nomdagi maydon uchun primary key berish uchun.
+        public int Id { get; set; } // Id yoki CategoryId bo'lsa, primary key avtomatik beriladi
+        [Required]
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+    }
+}   
