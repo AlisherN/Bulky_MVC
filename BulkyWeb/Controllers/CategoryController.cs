e4abe4668babcc37ef6dbafd849e930ba63494f0
Nasrullayev Alisher <myemail9445@inbox.ru>
2023-06-01 16:02:04 +0500

    categories edit and delete

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -45,5 +45,51 @@ namespace BulkyWeb.Controllers
 
 			return View();
         }
+
+		public IActionResult Edit(int? id)
+		{
+			if (id == null || id <= 0) 
+			{
+				return NotFound();
+			}
+
+            Category? category = _db.categories.Find(id);
+			//Category? category1 = _db.categories.FirstOrDefault(c => c.Name == "Thriller"); // 2-usul
+			//Category? category1 = _db.categories.Where(c => c.Id == id).FirstOrDefault(); // 3-usul
+
+			if (category == null)
+			{
+				NotFound();
+			}
+
+            return View(category);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(Category category)
+		{
+			if (ModelState.IsValid) 
+			{
+				_db.categories.Update(category);
+				_db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+			return View();
+		}
+
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePost(int? id)
+		{
+			Category? category = _db.categories.Find(id);
+			if (category == null)
+			{
+				NotFound();
+			}
+
+			_db.categories.Remove(category);
+			_db.SaveChanges();
+
+			return RedirectToAction("Index");
+		}
     }
 }
