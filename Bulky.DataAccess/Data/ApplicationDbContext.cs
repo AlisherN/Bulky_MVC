fc458246ba02ff82f724a8db1b8671d1a27f8f18
Nasrullayev Alisher <myemail9445@inbox.ru>
2023-06-02 16:15:42 +0500

    Turned to N-Tier

---
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Bulky.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Bulky.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected readonly IConfiguration _configuration;
+        public ApplicationDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_configuration.GetConnectionString("BulkyWebDatabase"));
+        }
+
+        public DbSet<Category> categories { get; set; }
+
+        // Seeder qilib databasega test ma'lumot qo'shish
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>().HasData(
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1 },
+                new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
+                new Category { Id = 3, Name = "History", DisplayOrder = 3 }
+                );
+        }
+    }
+}
